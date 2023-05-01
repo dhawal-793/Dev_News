c12b7857b47f306a6666dd13e4870140f8a32353
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-05-01 19:56:22 +0530

    Updated margins, styles for light and dark theme

--- components/SearchBox.tsx
+++ components/SearchBox.tsx
@@ -12,15 +12,15 @@ const SearchBox = () => {
         router.push(`/search?term=${input}`)
     }
     return (
-        <form onSubmit={handleSubmit} className="my-5 rounded-md px-3  max-w-6xl mx-auto flex gap-5 justify-between items-center border group group-focus-within:border-orange-500">
+        <form onSubmit={handleSubmit} className="mt-10 mb-5 md:mt-5 px-3 max-w-6xl mx-auto flex gap-5 justify-between items-center focus:text-orange-300 border-gray-500 dark:border-gray-100 border-b-2 md:border md:rounded-md">
             <input type="text"
                 placeholder="Search Keywords..."
-                className="w-full h-10  flex-1 outline-none bg-transparent placeholder:text-gray-400 text-gray-500 dark:text-orange-500"
+                className="w-full h-10  flex-1 outline-none bg-transparent placeholder:text-gray-400 text-zinc-600 dark:text-orange-400"
                 value={input}
                 onChange={(e) => setInput(e.target.value)}
             />
             <button type="submit"
-                className="h-10 text-orange-500 disabled:text-gray-400 "
+                className="h-10 text-orange-400 disabled:text-gray-400 "
                 disabled={!input}
             >SearchIcon
             </button>

369be82123f5c3602a2bc6985ba888c5408385c3
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-05-03 21:43:52 +0530

    Added types for Article, Pagination & newsResponse

--- typings.d.ts
+++ typings.d.ts
@@ -6,3 +6,28 @@ type Category =
     "science" |
     "sports" |
     "technology";
+
+type Article = {
+    author: string | null;
+    category: string;
+    country: string;
+    description: string;
+    image: string | null;
+    language: string;
+    published_at: string;
+    source: string;
+    title: string;
+    url: string;
+}
+
+type Pagination = {
+    count: Int;
+    limit: Int;
+    offset: Int;
+    total: Int;
+}
+
+type NewsResponse = {
+    pagination: Pagination;
+    data: Article[];
+}

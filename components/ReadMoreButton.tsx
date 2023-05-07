727ec756302802360d97bdc432427526ef5dd26e
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-05-07 19:21:25 +0530

    Updated ReadMoreButton to use Link instead of button

--- components/ReadMoreButton.tsx
+++ components/ReadMoreButton.tsx
@@ -1,21 +1,33 @@
 'use client'
 
-import { useRouter } from "next/navigation"
+import Link from "next/link"
 
 type Props = {
     article: Article
 }
 
 const ReadMoreButton = ({ article }: Props) => {
-    const router = useRouter()
-    const handleClick = () => {
-        const QueryString = Object.entries(article)
-            .map(([key, value]) => `${key}=${value}`).join("&")
-        const url = `/article?${QueryString}`
-        router.push(url)
-    }
+
+    // const router = useRouter()
+    // const queryString = Object.entries(article)
+    //     .map(([key, value]) => {
+    //         console.log('key ', key)
+    //         return (`${key}=${value}`)
+    //     }).join("&")
+    // const handleClick = () => {
+    //     const url = `/article?${queryString}`
+    //     console.log("Url =>", url);
+    //     // router.push(url)
+    // }
     return (
-        <button onClick={handleClick} className="bg-orange-400 h-10 rounded-b-lg dark:text-gray-900 hover:bg-orange-500">Read More</button>
+        <Link href={{
+            pathname: '/article',
+            query: article
+
+        }}
+            prefetch={false}
+            className="flex justify-center items-center bg-orange-400 h-10 rounded-b-lg dark:text-gray-900 hover:bg-orange-500">Read More</Link>
+        // <button onClick={handleClick} className="bg-orange-400 h-10 rounded-b-lg dark:text-gray-900 hover:bg-orange-500">Read More</button>
     )
 }
 

970d35f84c9a640811c634aa0e8f66d880634c98
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-05-07 20:51:08 +0530

    Updated Article page to check for valid url

--- app/article/page.tsx
+++ app/article/page.tsx
@@ -1,16 +1,32 @@
 'use client'
 import ArticlePage from "@/components/ArticlePage";
 import Link from "next/link";
-import { notFound } from "next/navigation"
+import { notFound, useSearchParams } from "next/navigation"
 
 type Props = {
     searchParams?: Article;
 }
 
 const Article = ({ searchParams }: Props) => {
-    if ((searchParams && Object.entries(searchParams).length === 0) || !searchParams) {
-        return notFound();
+
+    // if ((searchParams && Object.entries(searchParams).length === 0) || !searchParams) {
+    //     return notFound();
+    // }
+    const data = useSearchParams();
+    const articleData = {
+        title: data.has('title'),
+        author: data.has('author'),
+        category: data.has('category'),
+        country: data.has('country'),
+        description: data.has('description'),
+        image: data.has('image'),
+        language: data.has('language'),
+        published_at: data.has('published_at'),
+        url: data.has('url'),
+        source: data.has('source'),
     }
+    const isValidUrl = Object.values(articleData).every(item => item === true)
+    if (!isValidUrl) return notFound();
 
     return (
         <>

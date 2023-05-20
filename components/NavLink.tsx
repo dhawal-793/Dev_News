c9b81e99cf5db92e4a1a1fa00b90839e330bc973
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-05-20 21:05:44 +0530

    removed prefetch=false attribute from Link

--- components/NavLink.tsx
+++ components/NavLink.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 const NavLink = ({ category, isActive }: Props) => {
     return (
-        <Link href={`/news/${category}`} prefetch={false} className={`navlink ${isActive && "underline decoration-orange-400 underline-offset-4 font-bold "}`}>
+        <Link href={`/news/${category}`}  className={`navlink ${isActive && "underline decoration-orange-400 underline-offset-4 font-bold "}`}>
             {category}
         </Link>
     )

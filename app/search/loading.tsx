0a25537cb2a2359ceb1cd5485a17b712a0ca987e
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-05-23 11:44:09 +0530

    Updated the path for gridlayout component in all loading.tsx and NewsList.tsx

--- app/search/loading.tsx
+++ app/search/loading.tsx
@@ -1,4 +1,4 @@
-import GridLayout from "@/components/GridLayout";
+import GridLayout from "@/components/Layouts/GridLayout";
 import ArticleLoader from "@/components/loaders/ArticleLoader";
 
 export default function Loading() {

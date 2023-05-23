b4841bfcc9badafe0309866a55c42bed3418d8c6
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-05-23 11:51:33 +0530

    Added SearchNotFound when no search keyword data returned from api.

--- app/news/[category]/page.tsx
+++ app/news/[category]/page.tsx
@@ -12,8 +12,7 @@ const NewsByCategory = async ({ params: { category } }: Props) => {
   const news = await fetchNews(category)
   return (
     <>
-      <h1 className="capitalize border-b-2 pb-2 w-fit border-orange-400 font-serif font-semibold text-4xl
-            "> {category}</h1>
+      <h1 className="capitalize border-b-2 pb-2 w-fit border-orange-400 font-serif font-semibold text-4xl"> {category}</h1>
       <NewsList news={news} />
     </>
   )

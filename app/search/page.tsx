c5bc070745dc9d88d34f45766e955003fbd10a0e
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-05-25 13:28:21 +0530

    Added BackButton on search route

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,6 +1,7 @@
 import NewsList from "@/components/NewsList"
 import SearchNotFound from "@/components/Errors/SearchNotFound"
 import fetchNews from "@/lib/fetchNews"
+import BackButton from "@/components/BackButton"
 
 
 type Props = {
@@ -11,6 +12,7 @@ const SearchPage = async ({ searchParams }: Props) => {
     const news = await fetchNews('general', searchParams?.term, true)
     return (
         <>
+            <BackButton />
             <h1 className="capitalize border-b-2 pb-2 w-fit border-orange-400 font-serif font-semibold text-4xl
             ">Search Results for: {searchParams?.term}</h1>
             {news.data.length ?

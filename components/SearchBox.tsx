e3cfa966f7e24ff57fbc3eadb17ea8f183795d10
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-05-26 13:36:05 +0530

    Updated ErrorLayout and SearchBox to use custom colors.

--- components/SearchBox.tsx
+++ components/SearchBox.tsx
@@ -13,18 +13,18 @@ const SearchBox = () => {
         router.push(`/search?term=${input}`)
     }
     return (
-        <form onSubmit={handleSubmit} className="mt-10 mb-5 md:mt-5 px-3 max-w-6xl mx-auto flex gap-5 justify-between items-center focus:text-orange-300 border-gray-500 dark:border-gray-100 border-b-2 md:border md:rounded-md">
+        <form onSubmit={handleSubmit} className="mt-10 mb-5 md:mt-5 px-3 max-w-6xl mx-auto flex gap-5 justify-between items-center border-b-2 md:border border-dark-secondary dark:border-light md:rounded-md">
             <input type="text"
                 placeholder="Search Keywords..."
-                className="w-full h-10  flex-1 outline-none bg-transparent placeholder:text-gray-400 text-zinc-600 dark:text-orange-400"
+                className="w-full h-10  flex-1 outline-none bg-transparent placeholder:text-light-primary text-accent"
                 value={input}
                 onChange={(e) => setInput(e.target.value)}
             />
             <button type="submit"
-                className="h-10 text-orange-400 disabled:text-gray-400 "
+                className="h-10 text-accent disabled:text-light-primary "
                 disabled={!input}
             >
-                <MagnifyingGlassIcon className="w-5 h-5 "/>
+                <MagnifyingGlassIcon className="w-5 h-5 " />
             </button>
         </form>
     )

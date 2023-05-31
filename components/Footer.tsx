0fe4f471ee84d961734fa052c200396a517da4ed
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-05-31 11:05:37 +0530

    added a inner div with max-width of 6xl

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,13 +4,15 @@ const Footer = () => {
     const year = new Date().getFullYear().toString()
     return (
         <footer className="w-full p-5  border-t-2 border-light-primary dark:border-dark-primary
-        text-dark-primary dark:text-light-primary flex flex-col md:flex-row md:px-20 justify-between items-center gap-3">
-            <div className="text-xs">Copyright &copy; {year} <span className="text-accent">DevNews</span>
-            </div>
-            <div className="text-sm flex">
-                made with <HeartIcon className="mx-1 text-cancel w-4 h4" /> by <a href="https://dhawal-793-portfolio.onrender.com" target="_blank" className="ml-1 text-accent">
-                    Dhawal Vijayvargiya
-                </a>
+        text-dark-primary dark:text-light-primary ">
+            <div className="max-w-6xl mx-auto flex flex-col md:flex-row justify-between items-center gap-3">
+                <div className="text-xs">Copyright &copy; {year} <span className="text-accent">DevNews</span>
+                </div>
+                <div className="text-sm flex">
+                    Made with <HeartIcon className="mx-1 text-cancel w-4 h4" /> by <a href="https://dhawal-793-portfolio.onrender.com" target="_blank" className="ml-1 text-accent">
+                        Dhawal Vijayvargiya
+                    </a>
+                </div>
             </div>
         </footer>
     )

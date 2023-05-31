5a7d41bcec8d58db87798b6e274332943c538f54
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-05-31 11:06:15 +0530

    Added repository link to DevNews text.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
         <footer className="w-full p-5  border-t-2 border-light-primary dark:border-dark-primary
         text-dark-primary dark:text-light-primary ">
             <div className="max-w-6xl mx-auto flex flex-col md:flex-row justify-between items-center gap-3">
-                <div className="text-xs">Copyright &copy; {year} <span className="text-accent">DevNews</span>
+                <div className="text-xs">Copyright &copy; {year} <a href="https://github.com/dhawal-793/Dev_News" target="_blank" className="text-accent">DevNews</a>
                 </div>
                 <div className="text-sm flex">
                     Made with <HeartIcon className="mx-1 text-cancel w-4 h4" /> by <a href="https://dhawal-793-portfolio.onrender.com" target="_blank" className="ml-1 text-accent">

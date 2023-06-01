a8133626b0bc60822dfa200be4a4eeeba1c36c9e
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-06-01 14:29:42 +0530

    added extra-small breakpoint

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,6 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -7,28 +10,32 @@ module.exports = {
   ],
   darkMode: "class",
   theme: {
+    screens: {
+      'xs': '475px',
+      ...defaultTheme.screens,
+    },
     extend: {
       colors: {
         light: {
           DEFAULT: '#f3f4f6',
           primary: '#9ca3af',
-          secondary:'#6b7280'
+          secondary: '#6b7280'
         },
         dark: {
           DEFAULT: '#18181b',
           primary: '#27272a',
-          secondary:'#71717a'
+          secondary: '#71717a'
         },
         'article': {
           light: {
             DEFAULT: '#e2e8f0',
             primary: '#cbd5e1',
-            secondary:'#94a3b8'   
+            secondary: '#94a3b8'
           },
           dark: {
             DEFAULT: '#1e293b',
             primary: '#334155',
-            secondary:'#475569'
+            secondary: '#475569'
           }
         },
         'loading': {
@@ -36,13 +43,13 @@ module.exports = {
           dark: '#6b7280'
         },
         'accent': {
-          DEFAULT:'#fb923c',
+          DEFAULT: '#fb923c',
           primary: '#f97316',
-          secondary:'#1e293b'
+          secondary: '#1e293b'
         },
         'sun': '#f59e0b',
         'moon': '#1e3a8a',
-        'cancel':'#dc2626'
+        'cancel': '#dc2626'
 
       }
     }

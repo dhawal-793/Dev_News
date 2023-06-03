6bfe7ad909f3d8d7fe59efda0010ec40be835e39
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-06-03 13:03:52 +0530

    Bug fixed, added force-dynamic, modified required params

--- app/article/page.tsx
+++ app/article/page.tsx
@@ -8,40 +8,31 @@ type Props = {
     searchParams?: Article;
 }
 
-const checkValidUrl = (searchParams: Article) => {
+export const dynamic = 'force-dynamic';
+
+const checkValidUrl = (data: Article | undefined) => {
+    if (!data) return false
     const articleData = {
         title: {
-            has: 'title' in searchParams,
-            value: searchParams.title
+            has: 'title' in data,
+            value: data.title
         },
         category: {
-            has: 'category' in searchParams,
-            value: searchParams.category
-        },
-        country: {
-            has: 'country' in searchParams,
-            value: searchParams.country
+            has: 'category' in data,
+            value: data.category
         },
         description: {
-            has: 'description' in searchParams,
-            value: searchParams.description
-        },
-        language: {
-            has: 'language' in searchParams,
-            value: searchParams.language
+            has: 'description' in data,
+            value: data.description
         },
         published_at: {
-            has: 'published_at' in searchParams,
-            value: searchParams.published_at
+            has: 'published_at' in data,
+            value: data.published_at
         },
         url: {
-            has: 'url' in searchParams,
-            value: searchParams.url
-        },
-        source: {
-            has: 'source' in searchParams,
-            value: searchParams.source
-        },
+            has: 'url' in data,
+            value: data.url
+        }
     }
     return Object.values(articleData).every(item => item.has === true && item.value !== '')
 }
@@ -49,29 +40,32 @@ const checkValidUrl = (searchParams: Article) => {
 
 export async function generateMetadata({ searchParams }: Props) {
 
-    if (!searchParams || Object.entries(searchParams).length === 0) {
+    if (searchParams === undefined || (searchParams && Object.entries(searchParams).length === 0)) {
         return NOTFOUND_METADATA
     }
-    if (searchParams.title === null) return NOTFOUND_METADATA
 
     const isValidUrl = checkValidUrl(searchParams)
     if (!isValidUrl) return NOTFOUND_METADATA;
 
     return {
-        title: `${searchParams.title.toUpperCase()} | DEV NEWS`,
+        title: `${searchParams.title} | DEV NEWS`,
         description: `${searchParams.title}`,
     };
 }
 
 const Article = ({ searchParams }: Props) => {
 
-    if (!searchParams || Object.entries(searchParams).length === 0) {
+    if (searchParams === undefined || (searchParams && Object.entries(searchParams).length === 0)) {
+        console.log("searchParam not available");
         return notFound();
     }
     console.log("SearchParams=> ", searchParams);
     const isValidUrl = checkValidUrl(searchParams)
 
-    if (!isValidUrl) return notFound();
+    if (!isValidUrl) {
+        console.log("searchParam Invalid");
+        return notFound();
+    }
 
     return (
         <>

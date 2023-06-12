d0fc68cda998869f23c3cb134aabc3a19102fce6
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-06-12 12:43:59 +0530

    Moved constants and dummyNewsData into lib

---
+++ lib/constants.ts
@@ -0,0 +1,14 @@
+export const categories: Category[] = [
+    "general",
+    "business",
+    "entertainment",
+    "health",
+    "science",
+    "sports",
+    "technology"
+]
+
+export const NOTFOUND_METADATA = {
+    title: "404 Page Not Found",
+    description: "Requested Page is no longer here or never existed in the first place."
+}

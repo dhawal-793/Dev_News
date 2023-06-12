09546af4caaed6f25cb26cbd8655f7a61aa78575
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-06-12 12:52:01 +0530

    Added fetchNews to get latest data on first load.

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 import NewsList from "@/components/NewsList";
-import { dummyNews } from "@/lib/dummyNewsData";
+// import { dummyNews } from "@/lib/dummyNewsData";
 import { categories } from "@/lib/constants";
 import fetchNews from "@/lib/fetchNews";
 export default async function Home() {
-  const news = dummyNews();
-  // const news: NewsResponse = await fetchNews(categories.join(","));
+  // const news = dummyNews();
+  const news: NewsResponse = await fetchNews(categories.join(","));
   // console.log("News Data=>", news);
   // await new Promise(resolve => setTimeout(resolve, 3000))
 

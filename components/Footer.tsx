f8bee30c803f9a776f0b7aeaf710d5ac361226ec
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-06-29 10:45:19 +0530

    Updated Footer Link

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,13 +3,12 @@ import { HeartIcon } from "@heroicons/react/24/solid"
 const Footer = () => {
     const year = new Date().getFullYear().toString()
     return (
-        <footer className="w-full p-5 border-t-2 border-light-primary dark:border-dark-primary
-        text-dark-primary dark:text-light-primary ">
-            <div className="max-w-6xl mx-auto flex flex-col justify-between items-center gap-3">
+        <footer className="w-full p-5 border-t-2 border-light-primary dark:border-dark-primary text-dark-primary dark:text-light-primary ">
+            <div className="flex flex-col items-center justify-between max-w-6xl gap-3 mx-auto">
                 <small className="text-[0.6rem] xs:text-xs">Copyright &copy; {year} <a href="https://github.com/dhawal-793/Dev_News" target="_blank" className="text-accent">DevNews</a>
                 </small>
                 <small className="text-[0.7rem] xs:text-sm flex">
-                    Made with <HeartIcon className="mx-1 text-cancel w-4 h4" /> by <a href="https://dhawal-793-portfolio.onrender.com" target="_blank" className="ml-1 text-accent">
+                    Made with <HeartIcon className="w-4 mx-1 text-cancel h4" /> by <a href="https://dev-folio-793.vercel.app" target="_blank" className="ml-1 text-accent">
                         Dhawal Vijayvargiya
                     </a>
                 </small>

7a1bb950ea2b4d6c288226d1ef1a5ab7b746f50e
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-07-02 11:40:32 +0530

    Updated error for search page

--- app/search/error.tsx
+++ app/search/error.tsx
@@ -1,22 +1,9 @@
-'use client'; // Error components must be Client Components
+'use client' 
+ 
+import ServerError from '@/components/Errors/ServerError'
 
-
-import ServerError from '@/components/Errors/ServerError';
-import { useEffect } from 'react';
-
-export default function Error({
-    error,
-    reset,
-}: {
-    error: Error;
-    reset: () => void;
-}) {
-    useEffect(() => {
-        // Log the error to an error reporting service
-        console.error(error);
-    }, [error]);
-
-    return (
-        <ServerError />
-    );
+export default function Error() {
+  return (
+    <ServerError />
+  )
 }

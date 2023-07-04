b867f95749525e6416444806e46d08a476e804de
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-07-04 16:05:20 +0530

    Added autor, themecolor, keywords,creator,and url

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,31 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
-
+import { Metadata } from 'next'
 const inter = Inter({ subsets: ['latin'] })
 import Header from '../components/Header'
 import Providers from './Providers'
 import BackToTopButton from '@/components/BackToTopButton'
 import Footer from '@/components/Footer'
 
-export const metadata = {
-  title: 'DEV NEWS',
-  description: 'Get the Latest News Anytime Anywhere with DevNews.',
-  manifest: '/manifest.json'
+export const metadata: Metadata = {
+  title: 'THE DEV NEWS',
+  description: 'The Dev News - Get the Latest News Anytime Anywhere with DevNews.',
+  applicationName: 'THE DEV NEWS',
+  manifest: '/manifest.json',
+  metadataBase: new URL('https://dev-news-793.vercel.app'),
+  creator: 'Dhawal Vijayvargiya',
+  authors: [
+    {
+      name: 'Dhawal Vijayvargiya',
+      url: 'htps://github.com/dhawal-793'
+    }
+  ],
+  keywords: ['dev-news', 'open-source', 'dev', 'dev news', 'nextjs project', 'mediastck', 'news', 'news app', 'news web app', 'nextjs pwa app'],
+  themeColor: [
+    { media: '(prefers-color-scheme: light)', color: '#f3f4f6' },
+    { media: '(prefers-color-scheme: dark)', color: '#18181b' },
+  ],
+  
 }
 
 export default function RootLayout({
